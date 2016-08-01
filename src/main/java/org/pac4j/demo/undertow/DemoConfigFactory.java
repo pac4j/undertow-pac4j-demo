48dc947b6ce375da1ac45eb6f9d913d3f367aef0
Jérôme LELEU <jerome.leleu@sfr.com>
2016-08-01 16:14:18 +0200

    remove useless import

--- src/main/java/org/pac4j/demo/undertow/DemoConfigFactory.java
+++ src/main/java/org/pac4j/demo/undertow/DemoConfigFactory.java
@@ -6,7 +6,6 @@ import org.pac4j.core.client.Clients;
 import org.pac4j.core.client.direct.AnonymousClient;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.config.ConfigFactory;
-import org.pac4j.core.matching.ExcludedPathMatcher;
 import org.pac4j.http.client.direct.DirectBasicAuthClient;
 import org.pac4j.http.client.direct.ParameterClient;
 import org.pac4j.http.client.indirect.FormClient;
